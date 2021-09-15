0ec8738667645fb3fa525a5622cfe3278ac91a24
nicknorthnetmedia <nick@northnetmedia.com>
2021-09-14 22:17:36 -0400

    commit before push

--- ws.js
+++ ws.js
@@ -3,38 +3,46 @@ const wsBoxes = document.querySelectorAll('.ws-box')
 const srcArea = 9;
 let pathArray = [];
 let dic;
+let uniqueArrays = []
 
 let randLetters = [
-    ['','','',''],
-    ['','','',''],
-    ['','','',''],
-    ['','','','']
+    // ['','','',''],
+    // ['','','',''],
+    // ['','','',''],
+    // ['','','','']
+    ['m','s','t','p'],
+    ['g','o','j','z'],
+    ['y','v','n','s'],
+    ['a','r','e','t']
 ]
 
-let counter = 0
-let row = 0
+// let counter = 0
+// let row = 0
 
-console.log(alphArr.length, 'LENGTH')
-wsBoxes.forEach((box, index) => {
-    const randNum = Math.floor(Math.random() * alphArr.length)
-    box.innerHTML = `<h2>${alphArr[randNum]}</h2>`;
+// console.log(alphArr.length, 'LENGTH')
+// wsBoxes.forEach((box, index) => {
+//     const randNum = Math.floor(Math.random() * alphArr.length)
+//     box.innerHTML = `<h2>${alphArr[randNum]}</h2>`;
 
-    randLetters[row][counter] = alphArr[randNum]
-    counter++
-    if(index === 3 || index === 7 || index === 11){
-        row++
-        counter = 0
-    }
-})
+//     randLetters[row][counter] = alphArr[randNum]
+//     counter++
+//     if(index === 3 || index === 7 || index === 11){
+//         row++
+//         counter = 0
+//     }
+// })
 
 const wordTrace = (row, col, newPath) => {
     let fin = false
+    const thePath = JSON.parse(JSON.stringify(newPath));
     const newPathLetters = newPath.map(obj => obj.letter)
     const letters = newPathLetters.join('').toUpperCase()
     const found = dic.find(word => word.startsWith(letters))
+    console.log('FIRST WORD TRACE CALLED')
+    uniqueArrays.push(thePath)
 
-    console.log('THIS IS THE NEW PATH', newPath)
-    console.log('THIS IS THE NEW PATH LETTERS', letters)
+    // console.log('THIS IS THE NEW PATH', newPath)
+    // console.log(letters)
     // console.log('THIS IS THE WORD WE FOUND', found)
 
     if(found === undefined){
@@ -43,29 +51,34 @@ const wordTrace = (row, col, newPath) => {
     }else{
         // console.log('we got in the else')
         if(col+1 < 4){
-            wordSearch(row, col+1, newPath)
+            console.log('going right')
+            wordSearch(row, col+1, thePath)
         }else{
             fin = true
         }
         if(row+1 < 4){
-            wordSearch(row+1, col, newPath)
+            console.log('going down')
+            wordSearch(row+1, col, thePath)
         } else{
             fin = true
         }
         if(row+1 < 4 && col-1 >= 0){
-            wordSearch(row+1, col-1, newPath)
+            console.log('going sw')
+            wordSearch(row+1, col-1, thePath)
         } else{
             fin = true
         }
         if(row+1 < 4 && col+1 < 4){
-            wordSearch(row+1, col+1, newPath)
+            console.log('going se')
+            wordSearch(row+1, col+1, thePath)
         } else{
             fin = true
         }
         if(col-1 >= 0){
-            const inPath = newPath.some(index => index.row == row && index.col == col-1 )
+            const inPath = thePath.some(index => index.row == row && index.col == col-1 )
             if(!inPath){
-            wordSearch(row, col-1, newPath)
+            console.log('going w')
+            wordSearch(row, col-1, thePath)
             } else{
                 fin = true
             }
@@ -73,9 +86,10 @@ const wordTrace = (row, col, newPath) => {
             fin = true
         }
         if(row-1 >= 0 && col-1 >= 0){
-            const inPath = newPath.some(index => index.row == row-1 && index.col == col-1 )
+            const inPath = thePath.some(index => index.row == row-1 && index.col == col-1 )
             if(!inPath){
-            wordSearch(row-1,col-1, newPath)
+            console.log('going nw')
+            wordSearch(row-1,col-1, thePath)
             } else{
                 fin = true
             }
@@ -83,9 +97,10 @@ const wordTrace = (row, col, newPath) => {
             fin = true
         }
         if(row-1 >= 0){
-            const inPath = newPath.some(index => index.row == row-1 && index.col == col )
+            const inPath = thePath.some(index => index.row == row-1 && index.col == col )
             if(!inPath){
-            wordSearch(row-1, col, newPath)
+            console.log('going n')
+            wordSearch(row-1, col, thePath)
             } else{
                 fin = true
             }
@@ -93,15 +108,17 @@ const wordTrace = (row, col, newPath) => {
             fin = true
         }
         if(row-1 >= 0 && col+1 < 4){
-            wordSearch(row-1, col+1, newPath)
+            console.log('going ne')
+            wordSearch(row-1, col+1, thePath)
         } else{
             fin = true
         }
 
-
-        
         if(fin){
-            pathArray.push(newPath)
+            const wordMade = dic.find(word => word == (letters))
+            if(wordMade){
+                pathArray.push(thePath)
+            }
         }
     }
 }
@@ -110,11 +127,11 @@ const wordSearch = (row, col, path=[]) => {
     if(path.length === 0){
         //Starting a word search on a certain cell
         path.push({letter: randLetters[row][col], row: row, col: col })
-        const newPath = Array.from(path)
+        let newPath = JSON.parse(JSON.stringify(path));
         wordTrace(row, col, newPath)
     } else{
         path.push({letter: randLetters[row][col], row: row, col: col })
-        const contPath = Array.from(path)
+        let contPath = JSON.parse(JSON.stringify(path));
         //check if the newly pushed letter is a word in the dictionary
         wordTrace(row, col, contPath)
     }
@@ -131,10 +148,12 @@ const appExec = async() => {
     await wordFetch()
     randLetters.forEach((row, rowIndex) => {
         row.forEach((letter,letterIndex) => {
+            console.log('WORD SEARCH BEING CALLED IN HERE', randLetters[rowIndex][letterIndex])
             wordSearch(rowIndex, letterIndex)
         })
     })
     console.log(pathArray)
+    console.log(uniqueArrays)
 }
 
 appExec()
